b2017bc8836a5f646c0db3c66f1e75aa204ca09e
marcocnc <marcocancelliere56@gmail.com>
2023-06-05 16:16:29 +0200

    header semi-structure

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layout.main')
+
+
+@section('content')
+
+<h1>test</h1>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <div class="header-container d-flex justify-content-between">
+
+                <!-- Logo -->
+                <div class="image">
+                    <img src="../assets/img/dc-logo.png" alt="dc-logo">
+                </div>
+
+                <!-- Links -->
+                <div class="h-link d-flex-cont-center-items-center">
+                    <nav>
+                        <ul class="d-flex">
+                            <li>
+                                 <a href="#">LINK</a>
+                            </li>
+                            <li>
+                                 <a href="#">LINK</a>
+                            </li>
+                            <li>
+                                 <a href="#">LINK</a>
+                            </li>
+                            <li>
+                                 <a href="#">LINK</a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+  </header>
+</body>
+</html>
